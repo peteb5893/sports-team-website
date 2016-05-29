7a22428b932515c8f6038c600a0e6ce9964aed52
Peter Bennington <p.j.bennington@dundee.ac.uk>
2016-05-29 22:41:15 +0100

    Created sessions and add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  get '/login' => 'admin/sessions#new'
+
   namespace :admin do
     resources :moderators, only: [:index, :edit, :update]
+    resources :sessions, only: [:new, :create, :destroy]
   end
 
 end
